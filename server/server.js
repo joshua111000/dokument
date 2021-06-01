1ea7f6149d760d47187636717c9542474816b5b3
karishmas6 <carishmashukla28@gmail.com>
2021-06-01 22:37:50 +0530

    Create docs in db

--- server/server.js
+++ server/server.js
@@ -15,11 +15,13 @@ const io = require('socket.io')(3001,{
     }
 })
 
+const defaultValue = ''
+
 io.on('connection', socket => {
     socket.on('get-document', documentId => {
-        const data = ''
+        const document = findOrCreateDocument(documentId)
         socket.join(documentId)
-        socket.emit('load-document', data)
+        socket.emit('load-document', document.data)
     
     socket.on('send-changes', delta => {
         // broad cast a message to everyone except us that changes have been made
@@ -27,3 +29,15 @@ io.on('connection', socket => {
         })
     })
 })
+
+const findOrCreateDocument = async (id) => {
+    if(id == null) return
+
+    const document = await Document.findById(id)
+    if (document) return document
+
+    return await Document.create({
+        _id: id,
+        data: defaultValue
+    })
+}
