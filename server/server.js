eea444fc13f2d42f0d8d73227d4d93a1cb7be6b8
karishmas6 <carishmashukla28@gmail.com>
2021-06-01 22:26:49 +0530

    Mongo connection

--- server/server.js
+++ server/server.js
@@ -1,3 +1,12 @@
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb://localhost/dokument", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+})
+
 const io = require('socket.io')(3001,{
     cors: {
         origin: 'http://localhost:3000',
